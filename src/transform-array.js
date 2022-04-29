ceb276989b18bdb443483c7cc0df9a7ee6fcb4d4
Eduard <eduard.nabiulin@gmail.com>
2022-04-29 11:29:04 +0300

    Transform array task is done

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,68 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+
+  const resultArr = [...arr];
+  let strIndex = [];
+
+  resultArr.forEach((item, index) => {
+    if (typeof item === 'string') {
+      strIndex.push(index);
+    }
+  });
+
+
+  strIndex.forEach(item => {
+    switch (resultArr[item]) {
+      case '--discard-next':
+        if (typeof resultArr[item + 1] !== 'undefined') {
+          delete resultArr[item];
+          delete resultArr[item + 1];
+        } else {
+          delete resultArr[item];
+        }
+        break;
+      case '--discard-prev':
+        if (typeof resultArr[item - 1] !== 'undefined') {
+          delete resultArr[item];
+          delete resultArr[item - 1];
+        } else {
+          delete resultArr[item];
+        }
+        break;
+      case '--double-next':
+        if (typeof resultArr[item + 1] !== 'undefined') {
+          resultArr.splice(item, 2, resultArr[item + 1], resultArr[item + 1]);
+        } else {
+          delete resultArr[item];
+        }
+        break;
+      case '--double-prev':
+        if (typeof resultArr[item - 1] !== 'undefined') {
+          resultArr.splice(item - 1, 2, resultArr[item - 1], resultArr[item - 1]);
+        } else {
+          delete resultArr[item];
+        }
+        break;
+    }
+  });
+
+  const rArr = [];
+
+  [...resultArr].forEach(item => {
+    if (typeof item !== 'undefined') {
+      rArr.push(item);
+    }
+  });
+
+  return rArr;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
+function getSeason(date = 0) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (date === 0) {
+    return 'Unable to determine the time of year!';
+    }
+  // } else if (!(date instanceof Date) || date.getMilliseconds() === 0) {
+  //   throw new Error ('Invalid date!');
+  // }
+
+  try {
+    if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1) {
+      return 'winter';
+    } else if (date.getMonth() > 1 && date.getMonth() < 5) {
+      return 'spring';
+    } else if (date.getMonth() > 4 && date.getMonth() < 8) {
+      return 'summer';
+    } else if (date.getMonth() > 7 && date.getMonth() < 11) {
+      return 'autumn';
+    }
+  } catch {
+    throw new Error ('Invalid date!');
+  }
 }
 
 module.exports = {

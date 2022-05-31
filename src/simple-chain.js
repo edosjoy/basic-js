97783552fbd2e7bd0e6675598e4a689451667c5e
Eduard <eduard.nabiulin@gmail.com>
2022-05-31 17:23:57 +0300

    The Chain maker task is done

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+
+  chain: '',
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    return this.chain.split('~~').length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+
+  addLink(value = '') {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    this.chain += this.chain === '' ? `( ${value} )` : `~~( ${value} )`;
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+
+  removeLink(position) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    if (!Number.isInteger(position) || position < 1 || this.chain === '' || position >= this.chain.split('~~').length) {
+      this.chain = '';
+      throw new Error('You can\'t remove incorrect link!');
+    } else {
+      const arrChain = this.chain.split('~~');
+      arrChain.splice(position - 1, 1);
+      this.chain = arrChain.join('~~');
+    }
+
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    this.chain = this.chain.split('~~').reverse().join('~~');
+
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    const result = this.chain;
+    this.chain = '';
+
+    return result;
   }
 };
 
